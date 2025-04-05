9e8551319e9f0f985f589140bd75f208c2069b28
jagansirigiri <83845863+jagansirigiri@users.noreply.github.com>
2025-04-04 20:13:11 -0400

    Update Main.java
    Added . at the end of sop

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package com.sai.four;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Running All Tests...");
+        System.out.println("Running All Tests....");
         System.out.println("###################################");
         PrescriptionList.runTests();
         System.out.println("###################################");
